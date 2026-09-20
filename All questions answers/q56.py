711f95f256e6cfc31383c6460718cd2ba043aa57
Aman Choure <amanchoure665@gmail.com>
2026-09-20 23:49:12 +0530

    solved some list problems

--- All questions answers/q56.py
+++ All questions answers/q56.py
@@ -4,4 +4,17 @@ a new list where each element is the sum of the corresponding elements from
 both original lists.
 """
 
+def sum_of_list(lst1,lst2):
+    new_list = []
+    n = len(lst1)
 
+    for i in range(0,n):
+       
+        total = lst1[i] + lst2[i]
+        new_list.append(total)
+    return new_list
+
+nums1 = [6, -5, 4, 2, 10, 91, -75, 49, 9]
+nums2 = [4, 1, 54, 76, 41, 85, 3, 44, 2]
+
+print(sum_of_list(nums1,nums2))
--- All questions answers/q57.py
+++ All questions answers/q57.py
@@ -4,4 +4,14 @@ whether it is sorted in ascending order. Print True if it is sorted,
 and False otherwise.
 """
 
+def check_asc_ord(lst):
+    n = len(lst)
+    for i in range(0,(n-1)):
+        if lst[i]>lst[i+1]:
+            return False
+            
+    return True
 
+nums1 = [6, -5, 4, 2, 10, 91, -75, 49, 9]
+nums2 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(check_asc_ord(nums2))
